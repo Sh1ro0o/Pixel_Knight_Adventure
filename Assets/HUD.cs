4eba3ffc517d525f5450049b3813be793e15d731
Sh1ro0o <david.pintar00@gmail.com>
2022-07-17 15:49:38 +0200

    added player hud displaying health and coins ammount

---
+++ Assets/HUD.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] Combatant player;
+    [SerializeField] Image fillImage;
+    [SerializeField] TextMeshProUGUI totalCoinsText;
+
+    private void Start()
+    {
+        slider.maxValue = player.getMaxHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this code removes the leftover red color in the health bar that is left by the fillImage in the slider
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        else if (slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+
+        //updates health bar
+        float fillValue = player.getCurrentHealth();
+        slider.value = fillValue;
+
+        //updates coins text
+        totalCoinsText.text = GameManager.gameManager.totalCoins.ToString();
+    }
+}
--- Assets/Scripts/Combat/Combatant.cs
+++ Assets/Scripts/Combat/Combatant.cs
@@ -93,4 +93,9 @@ public abstract class Combatant : MonoBehaviour
     {
         return maxHealth;
     }
+
+    public virtual int getCurrentHealth()
+    {
+        return currentHealth;
+    }
 }
